39c04ec4e36172c58b83ae3f13329c4e3b61844f
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-11-25 20:46:02 -0500

    changes

--- about_pyhton/info.py
+++ about_pyhton/info.py
@@ -1,2 +1,14 @@
-# // hhell now i am goint to tell you //
-print("hello wolrd")
+# //What is the python  ?
+# Nowadays,there are too many programming languages while they are totaly different with their syntaxes togahetr using in a projects 
+# Long story short, pyhton is an easy to learn  and powerful programming language. Python supports modules and packages, which encourages program modularity and code reuse.
+# We can use python for :
+# 1)web development(server-side),
+# 2)software development,
+# 3)mathematics
+# 4)system scripting.  //
+programming_language=input("Choose programming language : ")
+if programming_language == "Python" or "python":
+    print("Learn python now  !!!")
+else:
+    print("Hello another option ")
+
