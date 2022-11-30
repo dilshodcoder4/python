8612e715fc2c49e2b6634d82da65ba491e560cea
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-11-29 20:40:40 -0500

    new info

---
+++ variables/main.py/main.py
@@ -0,0 +1,16 @@
+# Variable name and value
+order=25
+
+# how to print out the value  by using print()-function
+print(order)
+
+
+# we can aslo print value directly 
+print(25)
+
+# however using variable name is good when we change the value  after fact
+# for example
+age=25
+print(age)
+age=45
+print(age)
