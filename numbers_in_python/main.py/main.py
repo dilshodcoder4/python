97ca126c9acfcf0a617709aa9d5f06b21fd08cdb
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-01 20:56:29 -0500

    new inforamtion

--- numbers_in_python/main.py/main.py
+++ numbers_in_python/main.py/main.py
@@ -1,5 +1,8 @@
 #Integers and Floating-Point Numbers
 #There are two main types of numbers in python  and that is integers or whole numbers and floating point numbers or numbers with decimal place  
+# For example :
+age =21 #integer
+PI=3.14159 #float
 # An integer is an whole number with no decimal places .For exapla  1 is an integer  but
 # 1.0 isn't ,The name for the integer data type is int ,which you can see with type()
 print(type(2))
@@ -9,9 +12,6 @@ int("25")
 #Like integers, floats can be created from floating-point literals or by converting a string to a float with float():
 float("1.25")
 # >>1.25
-# For example :
-age =21 #integer
-PI=3.14159 #float
 #Mathematics works just as normally , so we might have  a variable named math operation
 maths_operation=20+2*9/2-5
 # as long as we can verify this via printing it 
@@ -24,4 +24,3 @@ print(float_division)
 integer_division=21//4
 print(integer_division)
 
-#
