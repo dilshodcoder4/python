ec09a73708f1e7e1e10ec22dc3b339be9813a013
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-07 20:29:12 -0500

    newinfo

---
+++ stings/main.py
@@ -0,0 +1,18 @@
+#Very often in our programmes, we will be dealing with numbers but almost as often,if not more  we will want to deal with text
+#For example the user will be giving us some text  and we will be doing some processing on it  as well as we will be waiting some text out to the screen 
+#And that is where the next datatype comes in In python  we have got another type called strings that are used to contain text like characters
+my_string='hello world'
+print(my_string)
+string_with_quotes="Hello   it is me "
+another_with_quotes='he said "you are good boy"  !'
+multiline =""" Hello world
+My name is Dilshod 
+"""
+print(multiline)
+name= 'Jane'
+greeting='hello'+name
+print(greeting)
+age="25"
+print("you are "+age)
+age_as_str=str(age)
+print("You are "+ age_as_str)
