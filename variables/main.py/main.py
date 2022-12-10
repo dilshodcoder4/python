50c0085fe21c672cba27eb98a059457014fcd8d4
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-10 13:17:47 -0500

    changes

--- variables/main.py/main.py
+++ variables/main.py/main.py
@@ -14,3 +14,14 @@ age=25
 print(age)
 age=45
 print(age)
+
+# In fact,Variables can include letters,numbers  and underscores while they can not start with numbers
+# when longer variables names are written in  snake_case in Pyhton
+
+father_age=50
+team_number=3
+
+# Variable names you will never change are written in all uppercase 
+
+PI=3.14159
+RADIANS_TO_DEGREES=180/PI
