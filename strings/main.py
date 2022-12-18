48e9ce2244c9e947c5705d879eafaf0ce03b157f
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-17 20:28:10 -0500

    changes

--- strings/main.py
+++ strings/main.py
@@ -1,125 +1,125 @@
 #Very often in our programmes, we will be dealing with numbers but almost as often,if not more  we will want to deal with text
 #For example the user will be giving us some text  and we will be doing some processing on it  as well as we will be waiting some text out to the screen 
 #And that is where the next datatype comes in In python  we have got another type called strings that are used to contain text like characters
-my_string='Hello world'
-print(my_string)
-#Hello world
+# my_string='Hello world'
+# print(my_string)
+# #Hello world
 
-# <<<------------>>>
-string_with_quotes="Hello   it is me "
-another_with_quotes='he said "you are good boy"  !'
-multiline =""" Hello world
-My name is Dilshod 
-"""
-print(multiline)
-# >>>   Hello world
-# My name is Dilshod
+# # <<<------------>>>
+# string_with_quotes="Hello   it is me "
+# another_with_quotes='he said "you are good boy"  !'
+# multiline =""" Hello world
+# My name is Dilshod 
+# """
+# print(multiline)
+# # >>>   Hello world
+# # My name is Dilshod
 
-#The + Operator
-# <<<------------>>>
-a="Hello"
-b="Dilshod"
-c="How are you "
-greeting=a + b + c
-print(greeting)
-#>>>Hello Dilshod How are you 
-# The * Operator
-c='you '
-v='me'
-d='we'
-print(c*4)
-#>>>youyouyouyou
-# The in Operator
-s='hello'
-b='hello world how are you '
-print('hello ' in b )
-print(s in b)
-# >>>True
-# >>>True
-# There is also a not in operator, which does the opposite:
-s='hello'
-b='hello world how are you '
-print('hello '  not in b )
-print(s not in b)
-#>>> False
-#>>> False
+# #The + Operator
+# # <<<------------>>>
+# a="Hello"
+# b="Dilshod"
+# c="How are you "
+# greeting=a + b + c
+# print(greeting)
+# #>>>Hello Dilshod How are you 
+# # The * Operator
+# c='you '
+# v='me'
+# d='we'
+# print(c*4)
+# #>>>youyouyouyou
+# # The in Operator
+# s='hello'
+# b='hello world how are you '
+# print('hello ' in b )
+# print(s in b)
+# # >>>True
+# # >>>True
+# # There is also a not in operator, which does the opposite:
+# s='hello'
+# b='hello world how are you '
+# print('hello '  not in b )
+# print(s not in b)
+# #>>> False
+# #>>> False
 
-# <<<------------>>>
-name= 'Jane'
-greeting='hello'+name
-print(greeting)
-#>>>hello Jane
+# # <<<------------>>>
+# name= 'Jane'
+# greeting='hello'+name
+# print(greeting)
+# #>>>hello Jane
 
-# <<<------------>>>
-age="25"
-print("you are "+age)
-#>>>  you are 25
-age_as_str=str(age)
-print("You are "+ age_as_str)
-#>>>  You are 25
+# # <<<------------>>>
+# age="25"
+# print("you are "+age)
+# #>>>  you are 25
+# age_as_str=str(age)
+# print("You are "+ age_as_str)
+# #>>>  You are 25
 
-# <<<------------>>>
-age=25
-print(f"You are {age}")
-#>>>  You are 25
+# # <<<------------>>>
+# age=25
+# print(f"You are {age}")
+# #>>>  You are 25
 
-# <<<------------>>>
-name="Dilshod"
-greeting=f"How are you {name}"
-print(greeting)
-#>>> How are you Dilshod
+# # <<<------------>>>
+# name="Dilshod"
+# greeting=f"How are you {name}"
+# print(greeting)
+# #>>> How are you Dilshod
 
-# <<<------------>>>
-name="Dilshod"
-final_greeting="How are you {}"
-dilshod_greeting=final_greeting.format(name)
-print(dilshod_greeting)
-#>>> How are you Dilshod
+# # <<<------------>>>
+# name="Dilshod"
+# final_greeting="How are you {}"
+# dilshod_greeting=final_greeting.format(name)
+# print(dilshod_greeting)
+# #>>> How are you Dilshod
 
-# <<<------------>>>
-name="Javlon"
-dilshod_greeting=final_greeting.format(name)
-print(dilshod_greeting)
-#>>> How are you Javlon
+# # <<<------------>>>
+# name="Javlon"
+# dilshod_greeting=final_greeting.format(name)
+# print(dilshod_greeting)
+# #>>> How are you Javlon
 
-# <<<------------>>>
-name="Dilshod"
-final_greeting="How are you {name}"
-dilshod_greeting=final_greeting.format(name="Dilshod")
-print(dilshod_greeting)
-#>>> How are you Dilshod
+# # <<<------------>>>
+# name="Dilshod"
+# final_greeting="How are you {name}"
+# dilshod_greeting=final_greeting.format(name="Dilshod")
+# print(dilshod_greeting)
+# #>>> How are you Dilshod
 
-# <<<------------>>>
-name=input("Enter your name :")
-final_greeting="How are you {name}"
-dilshod_greeting=final_greeting.format(name=name)
-print(dilshod_greeting)
-#>>> Enter your name : Dilshod
-#>>> How are you Dilshod
-# Getting user input in Pyhton 
-first_name="Tom"
-friend_name=input("Enter your friend name : ")
-print(f"Hello {first_name}.My friend name is {friend_name}")
-#>>> Enter your  friend name : Javlon
-#>>> Hello Tom .My friend name is Javlon
+# # <<<------------>>>
+# name=input("Enter your name :")
+# final_greeting="How are you {name}"
+# dilshod_greeting=final_greeting.format(name=name)
+# print(dilshod_greeting)
+# #>>> Enter your name : Dilshod
+# #>>> How are you Dilshod
+# # Getting user input in Pyhton 
+# first_name="Tom"
+# friend_name=input("Enter your friend name : ")
+# print(f"Hello {first_name}.My friend name is {friend_name}")
+# #>>> Enter your  friend name : Javlon
+# #>>> Hello Tom .My friend name is Javlon
 
-# <<<------------>>>
-age=input('Enter your age :')
-age_num=int(age)
-print(f"You have lived for {age_num*12} months")
-#>>>Enter your age :4
-#>>>You have lived for 48 months
+# # <<<------------>>>
+# age=input('Enter your age :')
+# age_num=int(age)
+# print(f"You have lived for {age_num*12} months")
+# #>>>Enter your age :4
+# #>>>You have lived for 48 months
 
-# <<<------------>>>
-age=int(input('Enter your age :'))
-print(f"You have lived for {age*12} months")
-#>>>Enter your age :4
-#>>>You have lived for 48 months
+# # <<<------------>>>
+# age=int(input('Enter your age :'))
+# print(f"You have lived for {age*12} months")
+# #>>>Enter your age :4
+# #>>>You have lived for 48 months
 
-# <<<------------>>>
-age=int(input('Enter your age :'))
-months=age*12
-print(f"You have lived for {months} months")
+# # <<<------------>>>
+# age=int(input('Enter your age :'))
+# months=age*12
+# print(f"You have lived for {months} months")
 #>>>Enter your age :4
 #>>>You have lived for 48 months
 # Built-in String Functions
@@ -236,11 +236,64 @@ print(my_self.capitalize())
 # lower() returns a copy of my_self with all alphabetic characters converted to lowercase:
 my_self="Hello my name is Dilshod"
 print(my_self.lower())
+#>>>hello my name is dilshod
 
 
 
+# <<<------------>>>
+# swapcase()
+# swapcase() returns a copy of my_self with uppercase alphabetic characters converted to lowercase and vice versa:
+my_self="Hello my name is Dilshod"
+print(my_self.swapcase())
+#>>>hELLO MY NAME IS dILSHOD
 
 
+# <<<------------>>>
+# title()
+# title()  returns a copy of my_self in which the first letter of each word is converted to uppercase and remaining letters are lowercase:
+my_self="Hello my name is Dilshod"
+print(my_self.title())
+#>>>Hello My Name Is Dilshod
+ 
+
+# <<<------------>>>
+# upper()
+# upper()  returns a copy of my_self with all alphabetic characters converted to uppercase:
+my_self="Hello my name is Dilshod"
+print(my_self.upper())
+#>>>HELLO MY NAME IS DILSHOD
+ 
+
+#Find and Replace
+# These methods provide various means of searching the target string for a specified substring.
+
+# <<<------------>>>
+# count()
+# count()  returns the number of non-overlapping occurrences of substring <sub> in my_self :
+my_self="Hello my name is Dilshod"
+print(my_self.count('i'))
+#>>> 2
+print(my_self.count('l'))
+#>>> 3
+
+# <<<------------>>>
+# endwith()
+# endwith()   returns True if my_self ends with the specified <suffix> and False otherwise:
+my_self="Hello my name is Dilshod"
+print(my_self.endswith('Dilshod'))
+#>>> True
+print(my_self.endswith('name'))
+#>>> False
+
+# <<<------------>>>
+# find()
+# find()  You can use .find() to see if a Python string contains a particular substring. my_self.find(<sub>) 
+# returns the lowest index in my_self where substring <sub> is found:
+my_self="Hello my name is Dilshod"
+print(my_self.find('my'))
+#>>> 6 
+print(my_self.find('lo'))
+#>>> 3
 
 
 
