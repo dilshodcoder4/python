8761f68531d8f4d15e42398aba913132551eb813
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-21 21:25:52 -0500

    changed

--- string_exercises_solution/main.py
+++ string_exercises_solution/main.py
@@ -1,6 +1,7 @@
-name="dilsod"
-x=len(name)
+# 2. Write a Python program to count the number of characters (character frequency) in a string.
+word=input("Enter word :")
+x=len(word)
 for n in range(x):
-    print(name[n])
-    b=name[n]
-    print(b.count())
+    print(f'{word[n]} >>> {word.count(word[n])}')
+   
+
