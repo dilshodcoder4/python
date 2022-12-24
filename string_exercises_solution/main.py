ff106346282150961b762772adc97bdc5a71006f
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-23 21:11:51 -0500

    changes

--- string_exercises_solution/main.py
+++ string_exercises_solution/main.py
@@ -1,3 +1,10 @@
+
+# 1. Write a Python program to calculate the length of a string.
+def calculate_length_of_string(strr):
+    print(len(strr))
+calculate_length_of_string('hello')    
+
+
 # 2. Write a Python program to count the number of characters (character frequency) in a string.
 word=input("Enter word :")
 x=len(word)
@@ -5,3 +12,12 @@ for n in range(x):
     print(f'{word[n]} >>> {word.count(word[n])}')
    
 
+# 3. Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. 
+# If the string length is less than 2, return instead of the empty string
+def swapchar(string):
+    if len(string)>2:
+        print(f'{string[0:2]}{string[-2]}{string[-1]}')
+    else:
+        return None
+
+swapchar('he')
