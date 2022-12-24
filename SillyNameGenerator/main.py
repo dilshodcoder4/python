4a901d5f425323b625ef48fc7d62ed08a7b17999
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2022-12-23 21:35:44 -0500

    changes

---
+++ SillyNameGenerator/main.py
@@ -0,0 +1,24 @@
+import sys, random
+print('Welcome to game !!!')
+FirstName = "jay", "jim", "roy", "axel", "billy", "charlie", "jax", "gina", "paul",
+"ringo", "ally", "nicky", "cam", "ari", "trudie", "cal", "carl", "lady", "lauren",
+"ichabod", "arthur", "ashley", "drake", "kim", "julio", "lorraine", "floyd", "janet",
+"lydia", "charles", "pedro", "bradley"
+LastName = "barker", "style", "spirits", "murphy", "blacker", "bleacher", "rogers",
+"warren", "keller"
+
+while True:
+    firstname=random.choice(FirstName)
+
+    lastName=random.choice(LastName)
+
+    print('\n\n')
+    print("{}{}".format(firstname,lastName),file=sys.stderr)    
+    print('\n\n')
+
+    try_again=input("""Please Try again :       
+                   Press  q to exist """)
+    if try_again.lower() == 'q':
+        try_again=0
+
+
