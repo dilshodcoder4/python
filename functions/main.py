4b22c240f830fe1d236f8f90315215960cfcc345
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-01-11 16:01:16 +0500

    changes

---
+++ functions/main.py
@@ -0,0 +1,138 @@
+#1
+# def greet():
+#     name=input("Enter your name :")
+#     print('Hello',name)
+
+# greet()
+
+# 2
+car={
+    "make":"Ford",
+    "model":"Fiesta",
+    "mileage":2300,
+    "fuel_consumed":456
+}
+
+mpg=car["mileage"]/car["fuel_consumed"]
+name=f"{car['make']} {car['model']}"
+print(f"{name} does {mpg} miles per gallon")
+
+# 3
+def calculate_mpg():
+    car={
+        "make":"Ford",
+        "model":"Fiesta",
+        "mileage":2300,
+        "fuel_consumed":456
+    }
+
+    mpg=car["mileage"]/car["fuel_consumed"]
+    name=f"{car['make']} {car['model']}"
+    print(f"{name} does {mpg} miles per gallon")
+
+calculate_mpg()    
+
+# 4
+def calculate_mpg(car_to_calculate):
+    mpg=car_to_calculate["mileage"]/car_to_calculate["fuel_consumed"]
+    name=f"{car_to_calculate['make']} {car_to_calculate['model']}"
+    print(f"{name} does {mpg} miles per gallon")
+
+
+calculate_mpg({
+        "make":"Ford",
+        "model":"Fiesta",
+        "mileage":2300,
+        "fuel_consumed":456
+    })
+# 5
+cars=[{"make":"Ford","model":"Fiesta","mileage":2300, "fuel_consumed":456},
+     {"make":"Bugatti","model":"Chevrolet","mileage":200, "fuel_consumed":120},
+     {"make":"Ferrari","model":"Daewoo","mileage":800, "fuel_consumed":200},
+     {"make":"Nexia","model":"Chevrolet_2","mileage":1200, "fuel_consumed":750}]
+
+def calculate_mpg(car,intro):
+    print(intro)
+    mpg=car["mileage"]/car["fuel_consumed"]
+    name=f"{car['make']} {car['model']}"
+    print(f"{name} does {mpg} miles per gallon")    
+
+
+
+for car in cars:
+    calculate_mpg(car,"New car")   
+
+# 6
+cars=[{"make":"Ford","model":"Fiesta","mileage":2300, "fuel_consumed":456},
+     {"make":"Bugatti","model":"Chevrolet","mileage":200, "fuel_consumed":120},
+     {"make":"Ferrari","model":"Daewoo","mileage":800, "fuel_consumed":200},
+     {"make":"Nexia","model":"Chevrolet_2","mileage":1200, "fuel_consumed":750}]
+
+def calculate_mpg(car):
+    mpg=car["mileage"]/car["fuel_consumed"]
+    return mpg
+
+def car_name(car):
+    name=f"{car['make']} {car['model']}"
+    return name
+
+def car_info(car):
+    name=car_name(car)
+    mpg=calculate_mpg(car)
+    print(f"{name} does {mpg} miles per gallon")    
+
+
+
+for car in cars:
+    mpg=car_info(car) 
+
+
+# 7
+def devide(x,y):
+    if y==0:
+        return "You tried to divide by zero"    
+    else:    
+        return x/y
+
+print(devide(70,5))
+
+
+# Default parameter values
+# 8
+def add(x,y):
+    total=x+y
+    return total 
+
+
+print(add(3,5))    
+
+# 9
+def add(x,y=4):
+    total=x+y
+    return total 
+
+
+print(add(3))    
+
+
+# 10
+def add(x,y):
+    total=x+y
+    return total 
+
+
+print(add(x=5,y=6))  
+
+# 11
+default_y=3
+
+
+def add(x,y=default_y):
+    total=x+y
+    print(total)
+
+
+add(2)
+
+
+
