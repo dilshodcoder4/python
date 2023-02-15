15daac2be84527507d1ab3b50cc19b77ca291a8f
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-02-15 06:41:32 +0500

    changes

---
+++ functions/art.py
@@ -0,0 +1,7 @@
+logo=""" 
+         ccccc  ii    pppp   h   h  eeeee  rrrr
+         c      ii    p   p  h   h  e      r   r
+         c      ii    pppp   hhhhh  eeee   rrrr
+         c      ii    p      h   h  e      r r
+         ccccc  ii    p      h   h  eeeee  r   r    
+         """
