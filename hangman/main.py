e896330e060cf2cce96ec64f7b5644a1720142b9
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-03-07 04:43:49 +0500

    changes

--- hangman/main.py
+++ hangman/main.py
@@ -1,42 +1,12 @@
-stages=['''
-     +---+
-         |
-         |
-         |
-        ===''', '''
-     +---+
-     O  |
-        |
-        |
-       ===''', '''
-    +---+
-    O   |
-    |   |
-        |
-      ===''', '''
-    +---+
-    O   |
-   /|   |
-        |
-       ===''', '''
-    +---+
-    O   |
-   /|\  |
-        |
-       ===''', '''
-    +---+
-    O   |
-   /|\  |
-  /     |
-       ===''', '''
-   +---+
-   O   |
-  /|\  |
-  / \  |
-      ===''']
-word_list=["cherry",'lion','camel']
+
 import random
+from hangmanwords import word_list
 chosen_word=random.choice(word_list)
+lives=6
+
+from hangmanlogo import logo
+print(logo)
+
 print(f"Psst,the solution is {chosen_word}")
 display=[]
 word_lengt=len(chosen_word)
@@ -46,15 +16,22 @@ print(display)
 end_of_game=False
 
 while not end_of_game:
-
-    guess=input("Guess a letter :").lower()
-
+    guess=input("Guess a letter :").lower()    
+    if guess in display:
+        print(f"You have already guessed {guess}")
     for possition in range(word_lengt):
         letter=chosen_word[possition]
         if letter==guess:
-            display[possition]=letter
-        
-    print(display)
+            display[possition]=letter           
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word.you lose a life.")
+        lives-=1
+        if lives==0:
+            end_of_game=True
+            print("you lose")            
+    print(f"{''.join(display)}")
     if "_" not in display:
         end_of_game=True
         print("You win. ")
+    from hangmanarts import stages    
+    print(stages[lives])    
