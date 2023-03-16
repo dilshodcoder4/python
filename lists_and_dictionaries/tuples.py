2314b9322b171373b5367bbf9eaa7d1f8c18a916
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-03-16 14:32:47 +0500

    changes

--- lists_and_dictionaries/tuples.py
+++ lists_and_dictionaries/tuples.py
@@ -10,3 +10,19 @@ friends=("Sanjar","Omadbek","Asal")
 friends=friends+("Nurillo",)
 print(friends)
 #>>>('Sanjar', 'Omadbek', 'Asal', 'Nurillo')
+
+
+# Change values
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+
+# Convert the tuple into a list, add "orange", and convert it back into a tuple:
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
