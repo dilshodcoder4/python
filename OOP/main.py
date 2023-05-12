d773478ecd02ad39b60feaa59eb468bdd1ac4063
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-05-12 07:36:05 +0500

    changes

---
+++ OOP/main.py
@@ -0,0 +1,27 @@
+# OOP=>> Opbject-Oriented Programming
+# 1 What is Object-Oriented Programming 
+# 2 Class in Python  
+# 3 Python Obcejts(Instances)
+# 4 How to Define a Class in Python 
+# 5 Instanting Objects
+# 6 Object Inhertance 
+# 7 Conclusion
+# WHAT IS THE PROGRAM ?
+# Input data==>Manipulate data ==>Return
+# OBJECT ORIENTED PROGRAMMING 
+# * A very common programming paradigm (style designing software )
+# * Make developing large software projects and more intuitive
+# * Allows us think of complex software in terms of real world objects and their relationships to one another 
+# OBJECTS 
+# * An entinty thing in your program often a noun 
+# * Example of an object could be single person 
+# Properties 
+# * Name
+# * Age 
+# * Address
+# BEHAVIROS 
+# * Walk 
+# * Talk 
+# * Breathe 
+
+
