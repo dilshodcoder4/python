30e55bc2005e28972da5a10d06cf250b008c776a
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-05-19 04:27:29 +0500

    changes

--- OOP/main.py
+++ OOP/main.py
@@ -6,12 +6,14 @@
 # 5 Instanting Objects
 # 6 Object Inhertance 
 # 7 Conclusion
+
 # WHAT IS THE PROGRAM ?
 # Input data==>Manipulate data ==>Return
 # OBJECT ORIENTED PROGRAMMING 
 # * A very common programming paradigm (style designing software )
 # * Make developing large software projects and more intuitive
 # * Allows us think of complex software in terms of real world objects and their relationships to one another 
+
 # OBJECTS 
 # * An entinty thing in your program often a noun 
 # * Example of an object could be single person 
@@ -24,4 +26,33 @@
 # * Talk 
 # * Breathe 
 
+# CLASSES
+# * Classes are used to create obects
+# * We can use many, unique objects from a single classes
+# * Classed define a type 
+# * The procces of creating an objects from a class is called instantion .
+# Here we create a new veriable called my_name with a value of Austin.The variable
+# is actually a reference in order to create it , we instated the built in str class
+# exaple 
+my_name="Dilshod"
+# Example :Doors     
+# door class 
+# Properties:
+#     heigh 
+#     color 
+#     is_locked 
+    
+# Behaviors:
+#     open()
+#     closed()
+#     toggle_lock ()
+        
 
+#    door1                      door2  
+# heigh 89                    heigh 89      
+# color red                   color red
+# is_locked False             is_locked False
+
+# opnen()                     opnen() 
+# closed()                    closed()
+# toggle_lock()               toggle_lock()
