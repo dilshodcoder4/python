3b88f318b8f6a3e95a7933571c1199189b1664fb
dilshodcoder <dilshodbekabdurahimov83@gmail.com>
2023-05-24 05:58:15 +0500

    changes

--- hard_exercises/main.py
+++ hard_exercises/main.py
@@ -34,3 +34,12 @@ else:
     print(f"{number} is not armstrong nunber")    
     
     
+# Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument.
+def fuc(n):
+    if n==0:
+        return 1
+    else:
+        return n*fuc(n-1)    
+    
+c=int(input("Enter the number to calculate the factorial of it :"))
+print(fuc(c))
